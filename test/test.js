79101f8ddb0d078b7171b255c38f708c115f3bfb
Mugsy Carter <mugsycarter@gmail.com>
2016-10-19 17:28:00 -0700

    Testpage started.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const store = require('../lib/store');
 const assert = require('assert');
+const fs = require('fs');
 
 const elements = [
   {
@@ -22,4 +23,35 @@ const elements = [
   }
 ];
 
-store.saveFile(elements, 'elements');
+describe('store.saveFile', function(){
+    it('creates a new directory', function(done){
+     store.saveFile(elements, 'elements', storeTest);
+      fs.exists('/data/elements', (exists) => {
+        var z = (exists ? 'yes' : 'nope!');
+      });
+      assert.equal(z, 'yes');
+    });  
+  
+  // it.('creates JSON files in the directory', done => {
+
+  //   const callback = (err) => {
+  // });
+
+  // it.('creates the correct number of files', done => {
+
+  //   const callback = (err) => {
+  // });
+
+
+
+
+});
+
+function storeTest(){
+  console.log('Fart, error is ');
+};
+
+
+
+
+;
